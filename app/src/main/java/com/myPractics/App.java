e9bb3e5984b9a9fede3244576de882e36b98f00e
kmk3296 <kmk3296@gmail.com>
2022-01-17 22:10:30 +0900

    .

--- app/src/main/java/com/myPractics/App.java
+++ app/src/main/java/com/myPractics/App.java
@@ -3,12 +3,15 @@
  */
 package com.myPractics;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
+@RestController
+@SpringBootApplication
+public class App {
+
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
 }
